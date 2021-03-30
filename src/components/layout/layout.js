4d248b33b8c2a00e10d1d709137f5022a72d887e
IAndresI <andrei200277@gmail.com>
2021-03-30 17:11:39 +0300

    fix footer position

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -4,19 +4,21 @@ import Header from '../header';
 
 const Layout = ({children, title="World Rank"}) => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Head>
         <title>{title}</title>
         <link rel="icon" href="./favicon.ico"/>
       </Head>
-      <Header/>
-      <main className={styles.main}>
-        {children}
-      </main>
+      <div className={styles.content}>
+        <Header/>
+        <main className={styles.main}>
+          {children}
+        </main>
+      </div>
       <footer className={styles.footer}>
         IAndresI
       </footer>
-    </>
+    </div>
   );
 };
 
